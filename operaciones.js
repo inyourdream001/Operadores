8624071941cc6a30ef8241924f8c8d991da1fdd9
inyourdream001 <andres._super@outlook.com>
2018-12-12 09:26:18 -0500

    Este es la primera modificacion

---
+++ operaciones.js
@@ -0,0 +1,18 @@
+console.log("Menu:\nS.-Suma \nR.-Resta \nM.-Multiplicacion \nD.-Division ");
+var x=8,y=4
+console.log("Numeros:\n"+x,+y);
+var s="La suma es: "+(x+y);
+var r="La resta es: "+(x-y);
+var m="La multiplicacion es: "+(x*y);
+var d="La division es: "+(x/y);
+
+function imprimirResultado(operacion){
+  if(operacion.s){
+  }
+  if(operacion.r){
+  }
+  if(operacion.m){
+  }
+  if(operacion.d){
+  }
+}
